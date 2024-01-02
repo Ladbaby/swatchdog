71fa510fbb6590c51722ba39eeccc04abc1165fa
Ladbaby <1390265970@qq.com>
2024-01-02 22:55:41 +0800

    add ip address monitor

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use parse_duration::{parse as parse_duration};
 use reqwest::blocking::Client;
 use reqwest::Method;
 use reqwest::Url;
+// use get_if_addrs::get_if_addrs;
+use get_if_addrs::{get_if_addrs, IfAddr};
 
 #[derive(Parser, Debug, Clone)]
 #[command(author, version)]
@@ -17,6 +19,26 @@ struct Args {
     interval: Duration,
     #[arg(long, default_value = "false")]
     verbose: bool,
+    #[arg(long)]
+    interface: Option<String>, // specify the network interface for the IP message
+}
+
+fn get_interface_address(interface_name: &str) -> Option<String> {
+    if let Ok(if_addrs) = get_if_addrs() {
+        for if_addr in if_addrs {
+            eprintln!("{}", if_addr.name);
+            if if_addr.name == interface_name {
+                if let IfAddr::V4(v4_addr) = if_addr.addr {
+                    return Some(v4_addr.ip.to_string());
+                } else {
+                    eprintln!("Error: Interface '{}' is not an IPv4 address", interface_name);
+                    return None;
+                }
+                // return Some(if_addr.addr.ip().to_string());
+            }
+        }
+    }
+    None
 }
 
 fn main() {
@@ -31,11 +53,24 @@ fn main() {
         let args = args.clone();
 
         thread::spawn(move || {
+            let mut url = args.url.clone();
+
+            // If the interface name is provided, fetch its IPv4 global address
+            if let Some(interface_name) = &args.interface {
+                if let Some(interface_address) = get_interface_address(interface_name) {
+                    let query = format!("status=up&msg={}&ping=", interface_address);
+                    url.set_query(Some(&query));
+                } else {
+                    eprintln!("Error: Unable to retrieve IPv4 address for interface '{}'", interface_name);
+                    // return;
+                }
+            }
+            
             if args.verbose {
-                eprint!("Send request to {} ... ", args.url);
+                eprint!("Send request to {} ... ", url);
             }
 
-            let result = client.request(args.method, args.url).send().and_then(|res| res.error_for_status());
+            let result = client.request(args.method, url).send().and_then(|res| res.error_for_status());
 
             if let Err(err) = result {
                 eprintln!("Error: {}", err)
