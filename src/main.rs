fca1eea57f1c7ade70d49904107813c7450eb42f
Ladbaby <1390265970@qq.com>
2024-01-02 23:45:02 +0800

    fix for Linux

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::thread;
-use std::time::Duration;
+// use std::thread;
+// use std::time::Duration;
+use std::{sync::{Arc, Mutex}, thread, time::Duration};
 use clap::Parser;
 use parse_duration::{parse as parse_duration};
 use reqwest::blocking::Client;
@@ -26,7 +27,7 @@ struct Args {
 fn get_interface_address(interface_name: &str) -> Option<String> {
     if let Ok(if_addrs) = get_if_addrs() {
         for if_addr in if_addrs {
-            eprintln!("{}", if_addr.name);
+            // eprintln!("{}", if_addr.name);
             if if_addr.name == interface_name {
                 if let IfAddr::V4(v4_addr) = if_addr.addr {
                     return Some(v4_addr.ip.to_string());
@@ -44,22 +45,37 @@ fn get_interface_address(interface_name: &str) -> Option<String> {
 fn main() {
     let args = Args::parse();
     let client = Client::new();
+    let prev_ip_address: Arc<Mutex<Option<String>>> = Arc::new(Mutex::new(None));
 
     println!("swatchdog v{} started!", env!("CARGO_PKG_VERSION"));
 
     loop {
         let client = client.clone();
-
         let args = args.clone();
+        let prev_ip_address_clone = Arc::clone(&prev_ip_address);
 
         thread::spawn(move || {
             let mut url = args.url.clone();
+            let mut status = "up";
 
             // If the interface name is provided, fetch its IPv4 global address
             if let Some(interface_name) = &args.interface {
                 if let Some(interface_address) = get_interface_address(interface_name) {
-                    let query = format!("status=up&msg={}&ping=", interface_address);
+                    let mut prev_ip = prev_ip_address_clone.lock().unwrap();
+                    if let Some(prev_ip_str) = &*prev_ip {
+                        // Compare with the previous IP address
+                        if prev_ip_str != &interface_address {
+                            status = "pending";
+                        }
+                        else {
+                            status = "up";
+                        }
+                    }
+                    let query = format!("status={}&msg={}&ping=", status, interface_address);
                     url.set_query(Some(&query));
+
+                    // Update the previous IP address
+                    *prev_ip = Some(interface_address.clone());
                 } else {
                     eprintln!("Error: Unable to retrieve IPv4 address for interface '{}'", interface_name);
                     // return;
